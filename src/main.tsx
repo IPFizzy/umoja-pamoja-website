9743e6223acef1d823dc1c9f75363d43ce9fde46
Keon Bushman <138335528+IPFizzy@users.noreply.github.com>
2026-07-20 16:37:27 -0700

    Load compact editorial type

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "@fontsource/barlow-condensed/latin-400.css";
+import "@fontsource/barlow-condensed/latin-600.css";
+import "@fontsource/barlow-condensed/latin-700.css";
+import "@fontsource/bodoni-moda/latin-600.css";
+import "@fontsource/bodoni-moda/latin-700.css";
 import App from "./App";
 import "./styles.css";
 
